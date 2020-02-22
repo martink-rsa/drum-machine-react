10bc282b977290f76c1503dfcff331aa0b35567d
Martin Kruger <martin@martinkruger.com>
2020-02-22 20:14:48 +0200

    Added choke groups

--- src/Static/soundbank0.js
+++ src/Static/soundbank0.js
@@ -6,8 +6,8 @@ import Sound2 from '../Assets/Sounds/set0/hh.wav';
 import Sound3 from '../Assets/Sounds/set0/crash.mp3';
 import Sound4 from '../Assets/Sounds/set0/bass1.mp3';
 import Sound5 from '../Assets/Sounds/set0/chord1.mp3';
-import Sound6 from '../Assets/Sounds/set0/chord1.mp3';
-import Sound7 from '../Assets/Sounds/set0/chord1.mp3';
+import Sound6 from '../Assets/Sounds/set0/fx2.mp3';
+import Sound7 from '../Assets/Sounds/set0/fx3.mp3';
 import Sound8 from '../Assets/Sounds/set0/fx.mp3';
 
 const soundBank = [
@@ -36,42 +36,42 @@ const soundBank = [
     sound: new Howl({
       src: [Sound3],
     }),
-    name: 'FX',
+    name: 'Crash',
     choke: -1,
   },
   {
     sound: new Howl({
       src: [Sound4],
     }),
-    name: 'Brass 1',
+    name: 'Bass',
     choke: 1,
   },
   {
     sound: new Howl({
       src: [Sound5],
     }),
-    name: 'Brass 2',
-    choke: 1,
+    name: 'Chords',
+    choke: 2,
   },
   {
     sound: new Howl({
       src: [Sound6],
     }),
-    name: 'Melody 1',
-    choke: 2,
+    name: 'FX1',
+    choke: -1,
   },
   {
     sound: new Howl({
       src: [Sound7],
     }),
-    name: 'Melody 2',
-    choke: 2,
+    name: 'FX2',
+    choke: -1,
   },
   {
     sound: new Howl({
       src: [Sound8],
     }),
-    name: 'Glockenspiel',
+    name: 'FX3',
     choke: -1,
   },
 ];
