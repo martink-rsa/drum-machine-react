ab3ca2d3037d1a6b6bf6e0f066c6491c19676efd
Martin Kruger <martin@martinkruger.com>
2020-02-22 20:23:28 +0200

    Updates

---
+++ precache-manifest.60822faa19a1165a96b607b8bac1b1f4.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3556a6eca45eda0b1d78ca0b4cd9a6d7",
+    "url": "/drum-machine-react/index.html"
+  },
+  {
+    "revision": "215d8408a5817d77270b",
+    "url": "/drum-machine-react/static/css/main.8f22b267.chunk.css"
+  },
+  {
+    "revision": "31f16c8f7510ca201718",
+    "url": "/drum-machine-react/static/js/2.997a8a19.chunk.js"
+  },
+  {
+    "revision": "b51e62edbcf7a9c4f4ad13f506021d3e",
+    "url": "/drum-machine-react/static/js/2.997a8a19.chunk.js.LICENSE"
+  },
+  {
+    "revision": "d9b29b536a181cf0bee5",
+    "url": "/drum-machine-react/static/js/3.d3c12ecb.chunk.js"
+  },
+  {
+    "revision": "d52f03d7f282dd2b4665",
+    "url": "/drum-machine-react/static/js/4.60c3a871.chunk.js"
+  },
+  {
+    "revision": "ce49b712963de981347d995b06a3ce7f",
+    "url": "/drum-machine-react/static/js/4.60c3a871.chunk.js.LICENSE"
+  },
+  {
+    "revision": "215d8408a5817d77270b",
+    "url": "/drum-machine-react/static/js/main.7f50530f.chunk.js"
+  },
+  {
+    "revision": "764ea736b67a100ffc4d",
+    "url": "/drum-machine-react/static/js/runtime-main.d1bfff11.js"
+  },
+  {
+    "revision": "1c36035081740a92631bebde5ce6ffdf",
+    "url": "/drum-machine-react/static/media/bass1.1c360350.mp3"
+  },
+  {
+    "revision": "5457102c35969fd8520e6ed58edbfc1d",
+    "url": "/drum-machine-react/static/media/chord1.5457102c.mp3"
+  },
+  {
+    "revision": "3192e135d4658e5de1bc038608ef8ab2",
+    "url": "/drum-machine-react/static/media/crash.3192e135.mp3"
+  },
+  {
+    "revision": "6668a5e28d06834d71654808ee190db8",
+    "url": "/drum-machine-react/static/media/fx.6668a5e2.mp3"
+  },
+  {
+    "revision": "99dec6411c93aaae92299e6aae91560b",
+    "url": "/drum-machine-react/static/media/fx2.99dec641.mp3"
+  },
+  {
+    "revision": "8ea2cec697852932d2c02b95e6f00d2e",
+    "url": "/drum-machine-react/static/media/fx3.8ea2cec6.mp3"
+  },
+  {
+    "revision": "cb69bb0e4f366d72615d7db839d0677d",
+    "url": "/drum-machine-react/static/media/hh.cb69bb0e.wav"
+  },
+  {
+    "revision": "1bc2fff3e581569ba85ce76b62df3c72",
+    "url": "/drum-machine-react/static/media/kick.1bc2fff3.wav"
+  },
+  {
+    "revision": "c1d98abb40fefa400d1f5f6db5123e7d",
+    "url": "/drum-machine-react/static/media/snare.c1d98abb.wav"
+  }
+]);
